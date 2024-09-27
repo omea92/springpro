983ff09f5ea2065a583c3d709e00ebf5e7ea0dae
user <user@DESKTOP-BB1C7TD>
2024-09-27 23:19:53 +0900

    롬복 적용 mybatis 조회 실습

--- springMVC01/src/main/java/kr/board/mapper/BoardMapper.java
+++ springMVC01/src/main/java/kr/board/mapper/BoardMapper.java
@@ -2,8 +2,11 @@ package kr.board.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import kr.board.entity.Board;
 
+@Mapper
 public interface BoardMapper { //SqlSessionFactory를 내부적으로 상속받아 구현
 	public List<Board> getLists(); //전체리스트 -> 메소드명과 동일한 SQL Mapper 필요
 }
