34ffe5fee3657673cda7715febfa264cb7c1e8da
user <thdjun92@naver.com>
2024-12-07 21:45:20 +0900

    DB 연동 반영(MYSQL)

--- SpringMVC01/src/main/java/kr/board/mapper/BoardMapper.java
+++ SpringMVC01/src/main/java/kr/board/mapper/BoardMapper.java
@@ -2,8 +2,11 @@ package kr.board.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import kr.board.entity.Board;
 
+@Mapper
 public interface BoardMapper {
 	List<Board> getLists();
 }
