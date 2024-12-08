458ef80d730090ab0e09d49ce288581f93a189f1
user <thdjun92@naver.com>
2024-12-08 13:44:13 +0900

    jackson-databind json 실습

--- SpringMVC02/src/main/java/kr/board/controller/BoardController.java
+++ SpringMVC02/src/main/java/kr/board/controller/BoardController.java
@@ -1,13 +1,31 @@
 package kr.board.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.board.entity.Board;
+import kr.board.mapper.BoardMapper;
 
 @Controller
 public class BoardController {
 	
+	@Autowired
+	BoardMapper mapper;
+	
 	@RequestMapping("/")
 	public String main() {
 		return "main";
+	}
+	
+	@RequestMapping("/boardList.do")
+	public @ResponseBody List<Board> boardList() {
+		
+		List<Board> list = mapper.getLists(); //목록 가져오기
+		
+		return list; //객체를 넘겨응답 = JSON 타입 반환 적합
 	}
 }
